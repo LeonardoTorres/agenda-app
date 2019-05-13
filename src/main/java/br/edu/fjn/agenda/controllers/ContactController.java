89c7a6764738c341499d5f7fffdb192a56881448
Leonardo Torres <leonardotorres9@gmail.com>
2019-05-13 20:35:06 -0300

    Initial Commit

---
+++ src/main/java/br/edu/fjn/agenda/controllers/ContactController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fjn.agenda.controllers;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.edu.fjn.agenda.domain.contact.Contact;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author leonardo
+ */
+@Controller
+@Path("contact")
+public class ContactController {
+    
+    @Inject
+    private Result result;
+    
+    
+    @Get("new")
+    public void newContact(){
+        
+    }
+    
+    @Post("save")
+    public void save(Contact contact){
+        //request.getParameter("name");
+        System.out.println("Contact's name " + contact.getPhoneNumber());
+         
+    }
+    
+   
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : home
+    Created on : May 6, 2019, 9:27:58 PM
+    Author     : leonardo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <c:forEach begin="0" end="9">
+            oi
+        </c:forEach>
+    </body>
+</html>
